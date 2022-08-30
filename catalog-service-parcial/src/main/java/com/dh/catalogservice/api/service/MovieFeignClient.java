90484fb6b5c4560d63551200e5c257b3f4b51e46
FacuKriva <facukriva2001@gmail.com>
2022-08-30 03:03:58 -0300

    Se agrega el LoadBalancer

--- catalog-service-parcial/src/main/java/com/dh/catalogservice/api/service/MovieFeignClient.java
+++ catalog-service-parcial/src/main/java/com/dh/catalogservice/api/service/MovieFeignClient.java
@@ -1,6 +1,8 @@
 package com.dh.catalogservice.api.service;
 
 import com.dh.catalogservice.domain.model.dto.MovieWS;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @FeignClient ( name = "movie-service" )
+@LoadBalancerClient( name = "movie-service", configuration = FeignAutoConfiguration.class)
 public interface MovieFeignClient {
         @GetMapping( " /movies/{genre} " )
         ResponseEntity < List<MovieWS> > findMovieByGenre(@PathVariable("genre") String genre);
