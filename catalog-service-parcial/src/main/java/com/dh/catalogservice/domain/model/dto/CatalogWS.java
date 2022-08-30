b42ed789a3c0a91b8165307aba234bb16f8a244e
FacuKriva <facukriva2001@gmail.com>
2022-08-30 03:01:54 -0300

    Se completan los model DTO's y se agrega la interfaz de 'ICatalogService.java'

--- catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/model/dto/CatalogWS.java
+++ catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/model/dto/CatalogWS.java
@@ -1,12 +1,36 @@
 package com.dh.catalogservice.domain.model.dto;
 
 import lombok.Builder;
-import lombok.Data;
 
 import java.util.List;
 
-@Data @Builder
+@Builder
 public class CatalogWS {
 	private String genre;
-	private List<MovieWS> movies;
+	private List< MovieWS > movies;
+
+	public CatalogWS(){
+		// No-args constructor
+	}
+
+	public CatalogWS( String genre, List< MovieWS > movies ){
+		this.genre = genre;
+		this.movies = movies;
+	}
+
+	public String getCatalogGenre() { return genre; }
+
+	public void setCatalogGenre( String genre ) { this.genre = genre; }
+
+	public List< MovieWS > getMovies() { return movies; }
+
+	public void setMovies( List< MovieWS > movies ) { this.movies = movies; }
+
+	@Override
+	public String toString() {
+		return " CatalogWS {"
+				+ " genre = " + genre
+				+ " , movies = " + movies
+				+ " } " ;
+	}
 }
--- catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/model/dto/MovieWS.java
+++ catalog-service-parcial/src/main/java/com/dh/catalogservice/domain/model/dto/MovieWS.java
@@ -1,4 +1,54 @@
 package com.dh.catalogservice.domain.model.dto;
 
 public class MovieWS {
+    private Integer id;
+    private String name;
+    private String genre;
+    private String urlStream;
+
+    public MovieWS() {
+        // No Args Constructor
+    }
+
+    public Integer getMovieId() {
+        return id;
+    }
+
+    public void setMovieId( Integer id ) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) { this.name = name; }
+
+    public String getMovieGenre() {
+        return genre;
+    }
+
+    public void setMovieGenre( String genre ) {
+        this.genre = genre;
+    }
+
+    public String getUrlStream() {
+        return urlStream;
+    }
+
+    public void setUrlStream( String urlStream ) {
+        this.urlStream = urlStream;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieWS { "
+                + "id = " + id
+                + ", movieName = " + name + '\''
+                + ", genre = " + genre + '\''
+                + ", urlStream = " + urlStream + '\''
+                + " }";
+    }
+
+
 }
