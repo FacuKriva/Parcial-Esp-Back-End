83a2abf11c9b5cfe21518db1f0658f5f6944a84c
FacuKriva <facukriva2001@gmail.com>
2022-08-30 21:00:17 -0300

    Se modifican los imports de los controllers

--- catalog-service-parcial/src/main/java/com/dh/catalogservice/api/controller/CatalogController.java
+++ catalog-service-parcial/src/main/java/com/dh/catalogservice/api/controller/CatalogController.java
@@ -1,29 +1,24 @@
 package com.dh.catalogservice.api.controller;
 
-import com.dh.catalogservice.api.service.ICatalogService;
+import com.dh.catalogservice.api.service.impl.CatalogServiceImpl;
 import com.dh.catalogservice.domain.model.dto.CatalogWS;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-
 @RestController
 @RequestMapping( "/catalogs" )
 public class CatalogController {
-	private final ICatalogService catalogService;
+	private final CatalogServiceImpl catalogService;
 
 	@Autowired
-	public CatalogController( ICatalogService catalogService ) {
-		this.catalogService = catalogService;
-	}
+	public CatalogController( CatalogServiceImpl catalogService ) { this.catalogService = catalogService; }
 
 	@GetMapping( "/{genre}" )
-	ResponseEntity< CatalogWS > getCatalogByGenre( @PathVariable String genre, HttpServletResponse response ) {
+	ResponseEntity< CatalogWS > getCatalogByGenre( @PathVariable String genre ) {
 		CatalogWS catalog = this.catalogService.findCatalogByGenre( genre );
 		return catalog == null
 				? ResponseEntity.notFound().build()
--- movie-service-parcial/src/main/java/com/dh/movieservice/api/controller/MovieController.java
+++ movie-service-parcial/src/main/java/com/dh/movieservice/api/controller/MovieController.java
@@ -3,31 +3,29 @@ package com.dh.movieservice.api.controller;
 import com.dh.movieservice.api.service.impl.MovieServiceImpl;
 import com.dh.movieservice.domain.model.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
 @RequestMapping( "/movies" )
 public class MovieController {
 
-	private final MovieServiceImpl movieServiceImpl;
+	private final MovieServiceImpl movieService;
 
 	@Autowired
-	public MovieController( MovieServiceImpl movieServiceImpl ) {
-		this.movieServiceImpl = movieServiceImpl;
+	public MovieController( MovieServiceImpl movieService ) {
+		this.movieService = movieService;
 	}
 
 	@GetMapping( "/{genre}" )
 	public ResponseEntity< List< Movie > > getMovieByGenre( @PathVariable String genre ) {
-		List< Movie > movieList = this.movieServiceImpl.getListByGenre( genre );
+		List< Movie > movieList = this.movieService.getListByGenre( genre );
 		return movieList.isEmpty() ? ResponseEntity.notFound().build(): ResponseEntity.ok().body( movieList );
 	}
 
 	@PostMapping
 	public ResponseEntity<Movie> saveMovie( @RequestBody Movie movie ) {
-		return ResponseEntity.ok().body( movieServiceImpl.save( movie ) );
+		return ResponseEntity.ok().body( movieService.save( movie ) );
 	}
 }
